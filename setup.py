05ceb6f2c0a6d200e0538ef3f55128c8ab312149
pr38 <57053686+pr38@users.noreply.github.com>
2020-05-17 22:01:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='dask_backward_feature_selection', 
     version='0.0.2', 
     description='Backward step-wise feature selection using Dask, scikit-learn compatible',
-    url='https://github.com/pypa/sampleproject', 
+    url='https://github.com/pr38/dask_backward_feature_selection', 
     install_requires=["numpy>=1.18.1","toolz>=0.9.0","scikit-learn>=0.20.1","dask>=2.6.0"],
     classifiers=[
     'Development Status :: 3 - Alpha',
