883ca9a04b572e899f9bb92e6b0ecf45ab1f0193
pr38 <57053686+pr38@users.noreply.github.com>
2020-12-21 12:03:01 -0800

    Update setup.py
    update for allowing sparse arrays

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 
     name='dask_backward_feature_selection', 
-    version='0.0.2', 
+    version='0.0.3', 
     description='Backward step-wise feature selection using Dask, scikit-learn compatible',
     url='https://github.com/pr38/dask_backward_feature_selection', 
     install_requires=["numpy>=1.18.1","toolz>=0.9.0","scikit-learn>=0.20.1","dask>=2.6.0"],
